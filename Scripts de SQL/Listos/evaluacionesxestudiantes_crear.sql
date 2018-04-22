a97a6ae96d7b1496aaa3bc8494992329f751b443
Ruben González Villanueva <rubengv1999@hotmail.com>
2018-04-22 05:54:43 -0600

    termino progra 1

--- Scripts de SQL/Listos/evaluacionesxestudiantes_crear.sql
+++
@@ -1,9 +0,0 @@
-use BBD1;
-go
-CREATE PROCEDURE evaluacionxestudiante_crear @idGrupoxEstudiante int, @idEvaluacion int, @Nota decimal(7, 4)
-AS
-BEGIN
-	INSERT INTO dbo.EvaluacionesxEstudiantes(idGrupoxEstudiante, idEvaluacion, Nota)
-	VALUES(@idGrupoxEstudiante, @idEvaluacion, @Nota)
-END
-go
--- Scripts de SQL/Listos/rubro_crear.sql
+++
@@ -1,9 +0,0 @@
-use BBD1;
-go
-CREATE PROCEDURE rubro_crear @nombre nvarchar(50)
-AS
-BEGIN
-	INSERT INTO dbo.Rubro(Nombre)
-	VALUES(@nombre)
-END
-go
